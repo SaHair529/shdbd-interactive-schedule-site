45a34cae7f6d248887156184ab2be334744fa18d
SaHair529 <sa.hairulaev@gmail.com>
2024-10-18 07:02:19 +0300

    Добавил защиту страниц от неавторизованных юзеров с помощью ProtectedRoute.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import SchedulePage from "./pages/SchedulePage/SchedulePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import {useState} from "react";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 
 function App() {
-    const [token, setToken] = useState(null)
+    const [token, setToken] = useState(() => localStorage.getItem('accessToken') || null)
+    console.log(token)
 
     return (
         <div className="App">
             <Router>
                 <Routes>
-                    <Route path="/schedule" element={<SchedulePage scheduleId={1}/>}/>
                     <Route path="/login" element={<LoginPage setToken={setToken}/> }/>
                     <Route path="/register" element={<RegisterPage/> }/>
+
+                    <Route path="/schedule" element={<ProtectedRoute token={token} element={<SchedulePage scheduleId={1} />} />}/>
                 </Routes>
             </Router>
         </div>
---
+++ src/pages/ProtectedRoute.js
@@ -0,0 +1,7 @@
+import {Navigate} from "react-router-dom";
+
+const ProtectedRoute = ({element, token}) => {
+    return token ? element : <Navigate to='/login' />
+}
+
+export default ProtectedRoute
