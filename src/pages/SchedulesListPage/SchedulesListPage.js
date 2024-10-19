2e25ff94d4d98739788da17d841767240b1b9d60
SaHair529 <sa.hairulaev@gmail.com>
2024-10-19 08:36:43 +0300

    Добавил норм верстку для страницы SchedulesListPage

--- src/pages/SchedulesListPage/SchedulesListPage.js
+++ src/pages/SchedulesListPage/SchedulesListPage.js
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import api from "../../api";
 import {useNavigate} from "react-router-dom";
-import {Card, CardContent, CircularProgress, Container, Grid2, Typography} from "@mui/material";
+import {Button, Card, CardContent, CircularProgress, Container, Divider, Grid2, Typography} from "@mui/material";
 
 
 const SchedulesListPage = ({token}) => {
@@ -41,13 +41,19 @@ const SchedulesListPage = ({token}) => {
             pt: 3
         }}>
             <Container>
+                <Typography variant="h4" gutterBottom color='text.primary'>Ваши расписания</Typography>
+                <Typography variant="body1" color="textSecondary" gutterBottom>
+                    Тут вы можете найти все доступные вам расписания.
+                </Typography>
                 <Grid2 container spacing={3}>
                     {schedules.map(schedule => (
                         <Grid2 item xs={12} sm={6} md={4} key={schedule.id}>
-                            <Card>
+                            <Card sx={{ boxShadow: 3 }}>
                                 <CardContent>
                                     <Typography variant="h6">{schedule.title}</Typography>
                                     <Typography color="textSecondary">ID: {schedule.id}</Typography>
+                                    <Divider sx={{ my: 2 }} />
+                                    <Button variant="contained" color="primary" size="small">Перейти к расписанию</Button>
                                 </CardContent>
                             </Card>
                         </Grid2>
@@ -55,7 +61,7 @@ const SchedulesListPage = ({token}) => {
                 </Grid2>
             </Container>
         </Container>
-    )
+    );
 }
 
 export default SchedulesListPage
