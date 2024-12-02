23fb96707c7191db45bbeb26aa3e2a13792d047c
SaHair529 <sa.hairulaev@gmail.com>
2024-12-02 18:40:46 +0300

    Загрузка юзеров из бд

--- src/pages/Admin/AdminUsersPage/AdminUsersPage.js
+++ src/pages/Admin/AdminUsersPage/AdminUsersPage.js
@@ -1,14 +1,78 @@
-import {TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Container, Box} from "@mui/material";
+import {
+    TableContainer,
+    Paper,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    Container,
+    Box,
+    Typography
+} from "@mui/material";
+import api from "../../../api";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import FullscreenLoader from "../../../components/FullscreenLoader";
+import {ErrorOutline} from "@mui/icons-material";
 
 
-const AdminUsersPage = () => {
-    const users = [ // todo брать юзеров из бд
-        { id: 1, fullName: 'Хайрулаев Шамиль', email: 'sa.hairulaev@gmail.com', roles: ['ROLE_USER', 'ROLE_ADMIN'] },
-        { id: 2, fullName: 'Алибеков Магомед', email: 'mn.alibekov@gmail.com', roles: ['ROLE_USER'] },
-        { id: 3, fullName: 'Алибеков Магомед', email: 'mn.alibekov@gmail.com', roles: ['ROLE_USER'] },
-        { id: 4, fullName: 'Алибеков Магомед', email: 'mn.alibekov@gmail.com', roles: ['ROLE_USER'] },
-        { id: 5, fullName: 'Алибеков Магомед', email: 'mn.alibekov@gmail.com', roles: ['ROLE_USER'] },
-    ]
+const AdminUsersPage = ({userSessionData}) => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        loadUsers()
+    }, userSessionData)
+
+    const loadUsers = async () => {
+        try {
+            const response = await api.get('/user', {
+                headers: {
+                    Authorization: `Bearer ${userSessionData['accessToken']}`
+                }
+            })
+            setUsers(response.data['users'])
+            setLoading(false)
+        }
+        catch (err) {
+            if (err.response.status === 401) {
+                localStorage.removeItem('userSessionData')
+                navigate('/login')
+                return
+            }
+
+            setError(err)
+        }
+    }
+
+    if (loading) {
+        return <FullscreenLoader loading={loading} />
+    }
+
+    if (error) {
+        return (
+            <Container maxWidth='xxl' sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                bgcolor: 'background.default'
+            }}>
+                <Paper elevation={3} sx={{
+                    padding: 3,
+                    textAlign: 'center',
+                    bgcolor: '#FFEBEE', // Светлый красный фон для ошибки
+                    color: '#D32F2F' // Красный цвет текста
+                }}>
+                    <ErrorOutline sx={{ fontSize: 40}} />
+                    <Typography variant="h6">{error}</Typography>
+                </Paper>
+            </Container>
+        );
+    }
 
     return (
         <Box sx={{ bgcolor: 'background.default', minHeight: '100vh' }}>
